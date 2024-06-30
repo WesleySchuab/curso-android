a538190d10761af92fa4b17a0554ee08e116d6bd
wesley <yinnihisoka@gmail.com>
2024-06-30 11:24:48 -0300

    Creating the book class

---
+++ app/src/main/java/com/example/myapplication/Livro.java
@@ -0,0 +1,9 @@
+package com.example.myapplication;
+
+public class Livro {
+private String titulo;
+private String autor;
+private String editora;
+private int paginas;
+private String localizacao;
+}
