3b0d7d65f64733a6fb934f73e589a2a3b30a109c
wesley <yinnihisoka@gmail.com>
2024-07-07 08:29:24 -0300

    criando a classe livro

---
+++ app/src/main/java/com/example/pookotlinencapsulamento/Livro.kt
@@ -0,0 +1,10 @@
+package com.example.pookotlinencapsulamento
+
+class book(
+    var tilte: String,
+    var author: String,
+    var publisher: String,
+    var location: String,
+    var page: Int
+) {
+}
